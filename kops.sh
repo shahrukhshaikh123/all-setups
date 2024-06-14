bf9aaea34f878ad5a204d1ef74641003ab7ff196
shahrukhshaikh123 <85749151+shahrukhshaikh123@users.noreply.github.com>
2024-06-14 08:51:47 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket cloudanddevopsbyshahrukh00309856.k8s.local --region ap-south-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyshahrukh00309856.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://cloudanddevopsbyshahrukh00309856.k8s.local
-kops create cluster --name shahrukh.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops create cluster --name shahrukh.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name shahrukh.k8s.local --yes --admin
